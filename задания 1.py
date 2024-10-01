9f89b6b593d774ec2d18fc12cd77a139ff31f51b
Arseniy2059 <semyashulkovy64@mail.ru>
2024-10-01 16:00:51 +0300

    Update задания 1.py

--- задания 1.py
+++ задания 1.py
@@ -1,23 +1,111 @@
 class User:
-    def init(self, nickname, password, age):
+    def __init__(self, nickname, password, age):
         self.nickname = nickname
-        self.nickname = nickname
-        self.password = password
+        self.password = hash(password)
         self.age = age
+
+    def __str__(self):
+        return self.nickname
+
+    def __eq__(self, other):
+        return self.nickname == other.nickname
+
+    def get_info(self):
+        return self.nickname, self.password
 
 
 class Video:
-    def init(self, title, duration, time_now, adult_mode,):
+    def __init__(self, title, duration, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.adult_mode = adult_mode
+        self.title_now = 0
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
 
 
 class UrTube:
-    def init(self):
+    def __init__(self):
+        self.user = []
+        self.videos = []
+        self.current_user = None
 
-     def log_in(self, nickname, password):
+    def pegister(self, nickname, password, age):
+        new_user = User(nickname, password, age)
+        if new_user not in self.users:
+            self.user.append(new_user)
+            self.current_user = new_user
+        else:
+            print(f"пользователя {nickname} уже существует")
+
+    def log_in(self, nickname, password, uesr=None):
+        for user in self.users:
+            if (nickname, hash(password)) == uesr.get_info():
+                self.current_user = user
+                break
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *files):
+        for film in files:
+            if film.title not in [video.title for video in self.videos]:
+                self.videos.append(film)
+
+    def get_videos(self, text: str):
+        files = []
+        for video in self.videos:
+            if text.upper() in video.title.upper():
+                files.append(video.title)
+        return files
+
+    def watch_video(self, film: str, time=None):
+        if self.current_user:
+            for video in self.videos:
+                if self.current_user and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
+                if film in video.title:
+                    for i in range(1, 11):
+                        print(i, end=' ')
+                        time.sleep(1)
+                        video.time_now += 1
+                    video.time_now = 0
+                    print('Конец видео')
+
+            else:
+                print('Войдите в аккаунт, чтобы смотреть видео')
+
+    def register(self, param, param1, param2):
+        pass
 
 
-    def register(self, nickname, password, age):
-        def log_out(self):
+if __name__ == '__main__':
+    ur = UrTube()
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-    def add(self):
-        def get_videos(self):
+    # Добавление видео
+    ur.add(v1, v2)
+
+    # Проверка поиска
+    print(ur.get_videos('лучший'))
+    print(ur.get_videos('ПРОГ'))
+
+    # Проверка на вход пользователя и возрастное ограничение
+    ur.watch_video('Для чего девушкам парень программист?')
+    ur.register('vasya_pumpkin', 'lolkekcheburek', 13)
+    ur.watch_video('Для чего девушкам парень программист?')
+    ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+    ur.watch_video('Для чего девушкам парень программист?')
+
+    # Проверка входа в другой аккаунт
+    ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+    print(ur.current_user)
+
+    # Попытка воспроизведения несуществующего видео
+    ur.watch_video('Лучший язык программирования 2024 года!')

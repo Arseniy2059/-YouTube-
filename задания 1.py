27f812a454f7f3c44d8e4e8fe510a7563b3b0215
Arseniy2059 <semyashulkovy64@mail.ru>
2024-10-02 14:31:41 +0300

    Update задания 1.py

--- задания 1.py
+++ задания 1.py
@@ -1,3 +1,5 @@
+import time
+
 class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
@@ -5,6 +7,9 @@ class User:
         self.age = age
 
     def __str__(self):
+        return self.nickname
+
+    def __repr__(self):
         return self.nickname
 
     def __eq__(self, other):
@@ -19,7 +24,7 @@ class Video:
         self.title = title
         self.duration = duration
         self.adult_mode = adult_mode
-        self.title_now = 0
+        self.time_now = 0
 
     def __str__(self):
         return self.title
@@ -30,14 +35,14 @@ class Video:
 
 class UrTube:
     def __init__(self):
-        self.user = []
+        self.users = []
         self.videos = []
         self.current_user = None
 
-    def pegister(self, nickname, password, age):
+    def rpegister(self, nickname, password, age):
         new_user = User(nickname, password, age)
         if new_user not in self.users:
-            self.user.append(new_user)
+            self.users.append(new_user)
             self.current_user = new_user
         else:
             print(f"пользователя {nickname} уже существует")
@@ -63,8 +68,10 @@ class UrTube:
                 files.append(video.title)
         return files
 
-    def watch_video(self, film: str, time=None):
-        if self.current_user:
+    def watch_video(self, film: str):
+        if self.current_user in None:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+        else:
             for video in self.videos:
                 if self.current_user and self.current_user.age < 18:
                     print("Вам нет 18 лет, пожалуйста покиньте страницу")
@@ -77,35 +84,29 @@ class UrTube:
                     video.time_now = 0
                     print('Конец видео')
 
-            else:
-                print('Войдите в аккаунт, чтобы смотреть видео')
-
-    def register(self, param, param1, param2):
-        pass
 
 
-if __name__ == '__main__':
-    ur = UrTube()
-    v1 = Video('Лучший язык программирования 2024 года', 200)
-    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-    # Добавление видео
-    ur.add(v1, v2)
+# Добавление видео
+ur.add(v1, v2)
 
-    # Проверка поиска
-    print(ur.get_videos('лучший'))
-    print(ur.get_videos('ПРОГ'))
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
 
-    # Проверка на вход пользователя и возрастное ограничение
-    ur.watch_video('Для чего девушкам парень программист?')
-    ur.register('vasya_pumpkin', 'lolkekcheburek', 13)
-    ur.watch_video('Для чего девушкам парень программист?')
-    ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-    ur.watch_video('Для чего девушкам парень программист?')
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
 
-    # Проверка входа в другой аккаунт
-    ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-    print(ur.current_user)
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
 
-    # Попытка воспроизведения несуществующего видео
-    ur.watch_video('Лучший язык программирования 2024 года!')
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
